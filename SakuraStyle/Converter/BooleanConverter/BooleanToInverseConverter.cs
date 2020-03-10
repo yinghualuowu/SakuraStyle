9ad4ee2b7c4ade81657fdb0ce61c9d485a84bc9d
yinghualuowu <1048146365@qq.com>
2020-03-10 16:49:39 +0800

    加一个ValueConversion

--- SakuraStyle/Converter/BooleanConverter/BooleanToInverseConverter.cs
+++ SakuraStyle/Converter/BooleanConverter/BooleanToInverseConverter.cs
@@ -1,5 +1,8 @@
-﻿namespace SakuraStyle.Converter.BooleanConverter
+﻿using System.Windows.Data;
+
+namespace SakuraStyle.Converter.BooleanConverter
 {
+    [ValueConversion(typeof(bool), typeof(bool))]
     public class BooleanToInverseConverter:BooleanConverter<bool>
     {
         public BooleanToInverseConverter() : base(true, false) { }
